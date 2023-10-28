2e100c8b095c6e474a4a5eae28768bf0e5af3dcf
Henrique <henrique2801lopes@gmail.com>
2023-10-28 19:13:22 -0300

    NavBar Changes

--- trem-expresso/src/components/NavBar.jsx
+++ trem-expresso/src/components/NavBar.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import TremExpressoImg from "../assets/TremExpresso.jpg";
+import "../assets/scss/NavBar.scss";
+import { faSearch }  from '@fortawesome/free-solid-svg-icons'
 
 function NavBar() {
-	return (
-		<nav className="navbar navbar-expand-lg navbar-light">
-			<div>
-				<a>
-					<img
-						src={TremExpressoImg}
-						alt="Trem Expresso imagem"
-						style={{ width: "128px", height: "auto" }}
-					/>
-					<span>Trem Expresso cafés</span>
-				</a>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className="d-flex align-items-center">
+        <a className="d-flex align-items-center">
+          <img
+            src={TremExpressoImg}
+            alt="Trem Expresso imagem"
+            style={{ width: "100px", height: "auto" }}
+          />
+        </a>
+        <span className="fs-1">Trem Expresso cafés</span>
+        <form role="search" className="d-flex">
+          <input type="text" placeholder="Search" className="form-control" />
+          <button className="btn btn-light" type="submit">
+              Search
+            </button>
+        </form>
+        
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
