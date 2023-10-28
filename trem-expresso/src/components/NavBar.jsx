66c6583684f10a75934d9b71e71aab9fdeabd886
johnpolsh <99343129+johnpolsh@users.noreply.github.com>
2023-10-28 16:50:39 -0300

    add image

--- trem-expresso/src/components/NavBar.jsx
+++ trem-expresso/src/components/NavBar.jsx
@@ -1,12 +1,13 @@
 
 import React from "react";
+import TremExpressoImg from "../../public/TremExpresso.jpg";
 
 function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
             <div>
                 <a>
-                    {/* <img>TremExpresso</img> */}
+                    <img src={TremExpressoImg} alt="Trem Expresso imagem" style={{ width: '128px', height: 'auto' }} />
                     <span>Trem Expresso cafés</span>
                 </a>
             </div>
