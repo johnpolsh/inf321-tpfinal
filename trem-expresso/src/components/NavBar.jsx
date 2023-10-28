b79652d0ffa3d23554bab47ea44c5e21533ce2c5
Henrique <henrique2801lopes@gmail.com>
2023-10-28 19:47:25 -0300

    Change navBar

--- trem-expresso/src/components/NavBar.jsx
+++ trem-expresso/src/components/NavBar.jsx
@@ -1,42 +1,44 @@
 import React from "react";
 import TremExpressoImg from "../assets/TremExpresso.jpg";
 import "../assets/scss/NavBar.scss";
-import { faSearch }  from '@fortawesome/free-solid-svg-icons'
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+
 function NavBar() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="mx-auto text-center">
-          <div className="d-flex align-items-center">
-            <a className="d-flex align-items-center">
-              <img className="align-self-start"
-                src={TremExpressoImg}
-                alt="Trem Expresso imagem"
-                style={{ width: "100px", height: "auto", margin: "0 10px" }}
-              />
-            </a>
-            {/* <span className="fs-1" style={{ margin: "0 10px" }}>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className="mx-auto text-center">
+        <div className="d-flex align-items-center">
+          <a className="d-flex align-items-center">
+            <img
+              className="align-self-start"
+              src={TremExpressoImg}
+              alt="Trem Expresso imagem"
+              style={{ width: "100px", height: "auto", margin: "0 10px" }}
+            />
+          </a>
+          {/* <span className="fs-1" style={{ margin: "0 10px" }}>
               Trem Expresso Cafés
             </span> */}
-            <form role="search" className="d-flex">
-              <input type="text" placeholder="Search" className="form-control" />
-  
-              <button
-                className="btn btn-light"
-                type="submit"
-                style={{ margin: "0 10px" }}
-              >
-                <FontAwesomeIcon icon={faSearch} />
-              </button>
-              <button className="btn btn-light">
-                <FontAwesomeIcon icon={faUser} />
-              </button>
-            </form>
-          </div>
+          <form role="search" className="d-flex">
+            <input type="text" placeholder="Search" className="form-control" />
+
+            <button
+              className="btn btn-light"
+              type="submit"
+              style={{ margin: "0 10px" }}
+            >
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
+            <button className="btn btn-light">
+              <FontAwesomeIcon icon={faUser} />
+            </button>
+          </form>
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
+}
 
 export default NavBar;
