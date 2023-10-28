392802e54cb073dc2942e845f772b275b6d7591a
johnpolsh <99343129+johnpolsh@users.noreply.github.com>
2023-10-28 16:40:07 -0300

    add bootstrap

--- trem-expresso/src/App.jsx
+++ trem-expresso/src/App.jsx
@@ -3,7 +3,6 @@ import CoffeeProduct from "./components/CoffeeProduct";
 import NavBar from "./components/NavBar";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
 	const [count, setCount] = useState(0);
--- trem-expresso/src/components/NavBar.jsx
+++ trem-expresso/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function NavBar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light">
             <div>
                 <a>
                     {/* <img>TremExpresso</img> */}
