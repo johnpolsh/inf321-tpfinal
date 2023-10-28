9b6ea949423d491d704a90fb9a45151041cf4b5d
johnpolsh <99343129+johnpolsh@users.noreply.github.com>
2023-10-28 16:16:38 -0300

    exemplos

---
+++ trem-expresso/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div>
+                <a>
+                    {/* <img>TremExpresso</img> */}
+                    <span>Trem Expresso cafés</span>
+                </a>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
