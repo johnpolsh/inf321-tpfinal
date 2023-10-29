2608d5d79d93b57cb5fb359be275e1d3a79df94d
johnpolsh <99343129+johnpolsh@users.noreply.github.com>
2023-10-28 21:03:25 -0300

    missing file fixed

--- trem-expresso/src/components/NavBar.jsx
+++ trem-expresso/src/components/NavBar.jsx
@@ -1,46 +1,7 @@
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-<<<<<<< Updated upstream
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-
-function NavBar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light">
-      <div className="mx-auto text-center">
-        <div className="d-flex align-items-center">
-          <a className="d-flex align-items-center">
-            <img
-              className="align-self-start"
-              src={TremExpressoImg}
-              alt="Trem Expresso imagem"
-              style={{ width: "100px", height: "auto", margin: "0 10px" }}
-            />
-          </a>
-          {/* <span className="fs-1" style={{ margin: "0 10px" }}>
-              Trem Expresso Cafés
-            </span> */}
-          <form role="search" className="d-flex">
-            <input type="text" placeholder="Search" className="form-control" />
-
-            <button
-              className="btn btn-light"
-              type="submit"
-              style={{ margin: "0 10px" }}
-            >
-              <FontAwesomeIcon icon={faSearch} />
-            </button>
-            <button className="btn btn-light">
-              <FontAwesomeIcon icon={faUser} />
-            </button>
-          </form>
-        </div>
-      </div>
-    </nav>
-  );
-=======
 import { Link } from "react-router-dom";
 import TremExpressoImg from "../assets/TremExpresso.jpg";
-import "../assets/scss/NavBar.scss";
 
 function NavBar() {
 	return (
@@ -77,7 +38,6 @@ function NavBar() {
 			</div>
 		</nav>
 	);
->>>>>>> Stashed changes
 }
 
 export default NavBar;
