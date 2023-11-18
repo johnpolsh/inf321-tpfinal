4511b4bb98122e44c48c64da034e44d11cf24ee1
H3nriqu3L <henrique2801lopes@gmail.com>
2023-11-17 21:21:42 -0300

    Add Filter (in progress)

--- trem-expresso/src/components/SearchBar.jsx
+++ trem-expresso/src/components/SearchBar.jsx
@@ -1,43 +1,52 @@
 import { Link } from "react-router-dom";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
-// function SearchBar() {
-//     return (
-//       <div className="input-group">
-//         <div className="form-outline">
-//           <input type="search" id="form1" className="form-control" />
-//           <label className="form-label" htmlFor="form1">
-//             Search
-//           </label>
-//         </div>
-//         <button type="button" className="btn btn-primary">
-//           <i className="fas fa-search"></i>
-//         </button>
-//       </div>
-//     );
-//   }
+function SearchBar() {
+	return (
+		<div
+			className="d-flex justify-content-end align-items-center"
+			style={{ marginRight: "55px" }}
+		>
+			<div
+				className="input-group"
+				style={{ maxWidth: "350px" }}
+			>
+				<div className="input-group-append dropdown ">
+					<button
+						className="text-dark btn btn-secondary dropdown-toggle border-0 rounded-0"
+						type="button"
+						id="categoryDropdown"
+						data-toggle="dropdown"
+						style={{ backgroundColor: "#9e642e" }}
+					>
+						Filtros
+					</button>
+				</div>
 
-  function SearchBar() {
-    return (
-      <div className="d-flex justify-content-end align-items-center" style={{ marginRight: '55px' }}>
-        <div className="input-group" style={{ maxWidth: "300px", position: 'relative' }}>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Pesquisar"
-            aria-label="Pesquisar"
-            aria-describedby="basic-addon2"
-            style={{ borderColor: '#9e642e' }}
-          />
-          <div className="input-group-append fas fa-search" style={{ backgroundColor: '#9e642e' }}>
-            <Link to="#" className="btn btn-outline-secondary border-0 rounded-0" type="button">
-              <FontAwesomeIcon style={{ color: 'black' }} icon={faSearch} />
-            </Link>
-          </div>
-        </div>
-      </div>
-    );
-  }
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Pesquisar"
+					aria-label="Pesquisar"
+					aria-describedby="basic-addon2"
+					style={{ borderColor: "#9e642e" }}
+				/>
+				<div
+					className="input-group-append fas fa-search"
+					style={{ backgroundColor: "#9e642e" }}
+				>
+					<Link
+						to="#"
+						className="btn btn-outline-secondary border-0 rounded-0"
+						type="button"
+					>
+						<FontAwesomeIcon style={{ color: "black" }} icon={faSearch} />
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+}
 export default SearchBar;
-
