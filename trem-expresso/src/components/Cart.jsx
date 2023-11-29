68e080e0e2f66b01171977156ae07cc8a08c21f5
H3nriqu3L <henrique2801lopes@gmail.com>
2023-11-29 15:08:23 -0300

    cart

---
+++ trem-expresso/src/components/Cart.jsx
@@ -0,0 +1,9 @@
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
+function Cart(){
+    return(
+        <FontAwesomeIcon style={{ marginRight: '20px'}}  icon={faCartShopping} />
+    );
+}
+
+export default Cart;
