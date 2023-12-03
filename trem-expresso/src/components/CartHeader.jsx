5d900318b32ce2f86a3cc4703d3b219e64b4043d
H3nriqu3L <henrique2801lopes@gmail.com>
2023-12-03 15:38:33 -0300

    Change color header

--- trem-expresso/src/components/CartHeader.jsx
+++ trem-expresso/src/components/CartHeader.jsx
@@ -5,7 +5,7 @@ function CartHeader() {
 	return (
 	  <div className="flex-column flex-md-row bg-white rounded p-3">
 		<div className="mx-auto my-3 text-center">
-		  <h2>Carrinho de Compras</h2>
+		  <h2 style={{color:'#b96c4d'}}>Carrinho de Compras</h2>
 		</div>
 		{/* <div className="ml-md-auto my-3 text-md-left">
 		  h2<strong>Produtos</strong>
