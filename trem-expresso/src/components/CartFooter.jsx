14769ab8a1e21e39e1efd397c6b68aa9da4c9890
H3nriqu3L <henrique2801lopes@gmail.com>
2023-12-03 15:36:19 -0300

    Add alert msg cupom

--- trem-expresso/src/components/CartFooter.jsx
+++ trem-expresso/src/components/CartFooter.jsx
@@ -2,7 +2,18 @@ import { faMinus, faPlus, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
-function CartFooter({price}) {
+function CartFooter({ price }) {
+	const [cupomMessage, setCupomMessage] = useState("");
+
+	const handleAplicarCupom = () => {
+        ////
+		setCupomMessage("CUPOM INVÁLIDO, EXPIRADO OU JÁ UTILIZADO");
+
+		setTimeout(() => {
+			setCupomMessage("");
+		}, 2000);
+	};
+
 	return (
 		<div className="d-flex flex-column flex-md-row justify-content-between align-items-center bg-white rounded p-3">
 			<div className="px-3 my-3 text-center">
@@ -16,16 +27,21 @@ function CartFooter({price}) {
 							aria-describedby="button-aplicar"
 							style={{ backgroundColor: "white", borderColor: "#a36f3e" }}
 						/>
+
 						<button
 							className="btn btn-primary"
 							type="button"
 							id="button-aplicar"
 							style={{ backgroundColor: "#a36f3e", borderColor: "#a36f3e" }}
+							onClick={handleAplicarCupom}
 						>
 							Aplicar
 						</button>
 					</div>
 				</div>
+			</div>
+			<div className="position-absolute bottom-50 start-50 translate-middle-x mt-2 text-danger">
+				{cupomMessage}
 			</div>
 
 			<div className="px-3 my-3 text-center">
--- trem-expresso/src/pages/CartPage.jsx
+++ trem-expresso/src/pages/CartPage.jsx
@@ -3,31 +3,34 @@ import CartFooter from "../components/CartFooter";
 import CartHeader from "../components/CartHeader";
 import TremExpressoimg from "../assets/TremExpresso.jpg";
 
+import DefaultLayout from "../layouts/Default";
+
 function CartPage() {
 	return (
 		<div>
-		<CartHeader/>
-		<CartItem
-			name={"Cafe Italiano"}
-			type={"Cafe Arabico"}
-			img={TremExpressoimg}
-			weight={500}
-			discount={10.0}
-			price={100.0}
-			maxCount={230}
-		/>
-		<CartItem
-			name={"Cafe Italiano"}
-			type={"Cafe Arabico"}
-			img={TremExpressoimg}
-			weight={500}
-			discount={10.0}
-			price={100.0}
-			maxCount={230}
-		/>
-		<CartFooter price={1000}/>
+			{/* <DefaultLayout> */}
+				<CartHeader />
+				<CartItem
+					name={"Cafe Italiano"}
+					type={"Cafe Arabico"}
+					img={TremExpressoimg}
+					weight={500}
+					discount={10.0}
+					price={100.0}
+					maxCount={230}
+				/>
+				<CartItem
+					name={"Cafe Italiano"}
+					type={"Cafe Arabico"}
+					img={TremExpressoimg}
+					weight={500}
+					discount={10.0}
+					price={100.0}
+					maxCount={230}
+				/>
+				<CartFooter price={1200.59} />
+			{/* </DefaultLayout> */}
 		</div>
-		
 	);
 }
 
