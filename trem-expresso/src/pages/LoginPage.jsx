8d3fe864415feecc9b9b7c4e9d0cc8d358517fca
H3nriqu3L <henrique2801lopes@gmail.com>
2023-12-10 16:21:44 -0300

    add navBar login & cadastro

--- trem-expresso/src/pages/LoginPage.jsx
+++ trem-expresso/src/pages/LoginPage.jsx
@@ -1,9 +1,12 @@
 import Login from "../components/Login";
+import DefaultLayout from "../layouts/Default";
 
 function LoginPage() {
-    return (
-        <Login></Login>
-    );
+	return (
+		<DefaultLayout>
+			<Login></Login>
+		</DefaultLayout>
+	);
 }
 
 export default LoginPage;
